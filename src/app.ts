c5a71d69e429f06d545edd5d2d15f360739fd55a
gusthavo.vieira <gusthavorangel@gmail.com>
2023-11-09 22:55:08 -0300

    fastify config

--- src/app.ts
+++ src/app.ts
@@ -7,4 +7,11 @@ const app = fastify({ logger: true })
 app.register(authenticateRoutes, { prefix: 'auth' })
 app.register(userRoutes, { prefix: 'user' })
 
+app.addHook('onRequest', (request, reply, done) => {
+  reply.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+  reply.header('Access-Control-Allow-Headers', '*')
+  reply.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+  done()
+})
+
 export { app }
